d23c59677553e880379be4dd4104224351742206
mimiyako3 <mimika.0425@icloud.com>
2025-10-09 00:27:43 +0900

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,22 @@
+//expresnpのモジュールを読み込む
+const express = require("express");
+// CORS ミドルウェアを読み込む
+const cors = require("cors");
+
+//expressをインスタンス化
+const app =express();
+//PORT番号を設定
+const PORT = 3000;
+// CORS を有効化
+app.use(cors()); 
+
+console.log("Hello World");
+
+app.get("/api",(req,res)=>{
+    res.json("Women's Hackathon in Kyoto");
+});
+
+//サーバーを起動する
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
